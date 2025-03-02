a7718fdf7aa6c1ec0cf9f098fe1b7b6b1aca31cb
Jorge León Salas <jorgemachu123@gmail.com>
2025-03-01 19:08:19 -0600

    Fixed another formatting thiong

--- src/app/components/Projects/Chronostrike.tsx
+++ src/app/components/Projects/Chronostrike.tsx
@@ -52,7 +52,7 @@ export function Chronostrike({ project, onBack }: ChronostrikeProps) {
           {/* Text first on mobile, second on desktop */}
           <div className="">
             <h2 className="text-2xl font-bold mb-4">Overview</h2>
-            <p>
+            <p className="md:text-lg">
               This was the first multiplayer game I worked on professionally, so my experience
               in that area was initially limited. However, I still managed to learn the fundamentals
               and implement various features. Since I had previously worked on audio for Swarm 2, 
@@ -66,7 +66,7 @@ export function Chronostrike({ project, onBack }: ChronostrikeProps) {
         <div className="flex flex-col md:flex-row md:items-center gap-6">
           <div className="md:w-1/2 order-1 md:order-2">
             <h2 className="text-2xl font-bold mb-4">Audio & Wwise Integration</h2>
-            <p>
+            <p className="md:text-lg">
               Led audio programming by developing a centralized audio manager to route Wwise events, ensuring a streamlined audio pipeline for the game’s timeline-based development and multiplayer functionality. 
               This system managers sound effects and voice-over priority, as well as a queueing sytem. 
               I closely collaborated with our audio designer to implement his desired 
@@ -87,7 +87,7 @@ export function Chronostrike({ project, onBack }: ChronostrikeProps) {
         <div className="flex flex-col md:flex-row md:items-center gap-6">
           <div className="md:w-1/2 order-1 md:order-2">
             <h2 className="text-2xl font-bold mb-4">Multiplayer & Vivox Integration</h2>
-            <p>
+            <p className="md:text-lg">
               I integrated Vivox voice chat to allow persistent communication in multiplayer mode, 
               even during scene loading, until players ended their session. I also became more 
               familiar with Fusion and used it to implement certain multiplayer-driven features, 
@@ -106,7 +106,7 @@ export function Chronostrike({ project, onBack }: ChronostrikeProps) {
         <div className="flex flex-col md:flex-row md:items-center gap-6">
           <div className="md:w-1/2 order-1 md:order-2">
             <h2 className="text-2xl font-bold mb-4">Other Highlights</h2>
-            <p>
+            <p className="md:text-lg">
             Developed editor-only functionality, including a Gizmos preview for enemy attacks based on the game's timeline. 
             I also implemented enemy behaviors, such as an aiming turret, and collaborated with artists to integrate their work—from VFX to updating all 3D weapon models. 
             Ensured weapon functionality, hand positions, SFX, and VFX remained consistent and bug-free.
